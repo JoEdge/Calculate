396961e106c7938e7a4c43383993657982cc3825
Joanna Edgell <joedgell2013@gmail.com>
2014-10-01 09:36:11 -0400

    oddity...

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
 
-/*var  numOne = document.getElementById('num_one');
-     numTwo = document.getElementById('num_two');
-     submit = document.getElementById('calculateBtn');
-     answer = document.getElementById('answer');
-
-     var result = function (x,y) {
-         var sum = x + y;
-
-         answer.innerHTML = sum;
-    };
-
-
-    submit.onclick = function ()    {
-       var a = (numOne.value);
-       var b = (numTwo.value);
-       result(a,b);
- };*/
-
  var  numOne = document.getElementById('num_one');
       numTwo = document.getElementById('num_two');
       submit = document.getElementById('calculateBtn');
       answer = document.getElementById('answer');
 
+
       submit.onclick = function() {
         var result = Number(numOne.value) + Number(numTwo.value);
         answer.innerHTML = result;
-
-      };

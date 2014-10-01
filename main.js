3c50933cc65b64ad4d8c6437edba7e6aa89a9bf0
Joanna Edgell <joedgell2013@gmail.com>
2014-10-01 09:41:49 -0400

    weirdness fixed

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 
-/*var  numOne = document.getElementById('num_one');
-     numTwo = document.getElementById('num_two');
-     submit = document.getElementById('calculateBtn');
-     answer = document.getElementById('answer');
-
-     var result = function (x,y) {
-         var sum = x + y;
-
-         answer.innerHTML = sum;
-    };
-
-
-    submit.onclick = function ()    {
-       var a = (numOne.value);
-       var b = (numTwo.value);
-       result(a,b);
- };*/
 
  var  numOne = document.getElementById('num_one');
       numTwo = document.getElementById('num_two');

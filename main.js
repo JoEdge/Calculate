a4241e32992fcafccb43049be1d21e0aec8c1cf2
Joanna Edgell <joedgell2013@gmail.com>
2014-10-06 20:20:18 -0400

    Extra work on assignment

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 
  var  numOne = document.getElementById('num_one');
       numTwo = document.getElementById('num_two');
-      submit = document.getElementById('calculateBtn');
+      add = document.getElementById('addBtn');
+      subtract = document.getElementById('subBtn');
+      multiply = document.getElementById('multBtn');
+      divide = document.getElementById('divBtn');
       answer = document.getElementById('answer');
 
 
-      submit.onclick = function() {
+      add.onclick = function() {
         var result = Number(numOne.value) + Number(numTwo.value);
         answer.innerHTML = result;
+};
+
+
+      subtract.onclick = function() {
+        var result = Number(numOne.value) - Number(numTwo.value);
+        answer.innerHTML = result;
+};
+
+      multiply.onclick = function() {
+          var result = Number(numOne.value) * Number(numTwo.value);
+          answer.innerHTML = result;
+};
+
+
+      divide.onclick = function() {
+            var result = Number(numOne.value) / Number(numTwo.value);
+            answer.innerHTML = result;
+
+};
